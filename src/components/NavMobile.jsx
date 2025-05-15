3ac1c7bd4fc4e19e0cdf3498e03796554dbab29a
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-05-15 14:22:40 +0200

    added function to close dropdown menu on click

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -5,11 +5,16 @@ import MenuItem from "./MenuItem"
 const NavMobile = () => {
   const [isOpen, setOpen] = useState(false)
 
+  // close menu when an item is clicked
+  const handleClick = () => {
+    setOpen((prev) => !prev)
+  }
+
   return (
     <div className="md:hidden">
       <Hamburger toggled={isOpen} size={32} toggle={setOpen} label="Show menu" rounded />
       {isOpen && (
-        <ul className="fixed top-20 left-0 right-0 bottom-0 z-2 flex flex-col items-end gap-8 text-lg bg-black p-8">
+        <ul className="fixed top-20 left-0 right-0 bottom-0 z-2 flex flex-col items-end gap-8 text-lg bg-black p-8" onClick={handleClick}>
           <MenuItem text="Popular" path="/popular" />
           <MenuItem text="Top Rated" path="/top_rated" />
           <MenuItem text="Now Playing" path="/now_playing" />
