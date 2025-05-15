1b9a132fcf309b4aa5ab59ebf887e4e13be6ee88
MalLunBar <malinelundgren1991@gmail.com>
2025-05-15 14:41:28 +0200

    install hamburger

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
-import { Loader } from "../components/Loader"
+// import { Loader } from "../components/Loader"
 
-const NotFound = () => {
-  return (
-    <Loader />
-  )
-}
+// const NotFound = () => {
+//   return (
+//     <Loader />
+//   )
+// }
