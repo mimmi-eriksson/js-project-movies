0869780e06bd84f9b551ac371ccfbe20d4406301
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-05-16 09:47:11 +0200

    added social buttons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,37 @@
+import SocialButton from "./SocialButton"
+
 const Footer = () => {
   return (
     <footer
       className="py-5"
     >
       <div
-        className="flex flex-col items-center gap-1"
+        className="flex items-center justify-center gap-5"
       >
-        <p
-          className="text-sm text-[#c44040]"
-        >&copy; {new Date().getFullYear()} Mimmi Eriksson</p>
-        <ul
-          className="flex gap-2"
-        >
-          <SocialButton link="https://www.linkedin.com/in/mimmi-aj-eriksson/" icon="/assets/linkedin-btn.svg" text="LinkedIn profile" />
-          <SocialButton link="https://github.com/mimmi-eriksson" icon="/assets/github-btn.svg" text="GitHub profile" />
-        </ul>
-
-        <p
-          className="text-sm text-[#c44040]"
-        >&copy; {new Date().getFullYear()} Malin Lundgren</p>
-        <ul
-          className="flex gap-2"
-        >
-          <SocialButton link="https://www.linkedin.com/in/malin-elisabeth-lundgren/" icon="/assets/linkedin-btn.svg" text="LinkedIn profile" />
-          <SocialButton link="https://github.com/MalLunBar" icon="/assets/github-btn.svg" text="GitHub profile" />
-        </ul>
+        <div className="flex flex-col items-center gap-1">
+          <p
+            className="text-sm text-white"
+          >Mimmi Eriksson</p>
+          <ul
+            className="flex gap-2"
+          >
+            <SocialButton link="https://www.linkedin.com/in/mimmi-aj-eriksson/" icon="/assets/linkedin-btn.svg" text="LinkedIn profile" />
+            <SocialButton link="https://github.com/mimmi-eriksson" icon="/assets/github-btn.svg" text="GitHub profile" />
+          </ul>
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <p
+            className="text-sm text-white"
+          >Malin Lundgren</p>
+          <ul
+            className="flex gap-2"
+          >
+            <SocialButton link="https://www.linkedin.com/in/malin-elisabeth-lundgren/" icon="/assets/linkedin-btn.svg" text="LinkedIn profile" />
+            <SocialButton link="https://github.com/MalLunBar" icon="/assets/github-btn.svg" text="GitHub profile" />
+          </ul>
+        </div>
       </div>
-    </footer>
+    </footer >
   )
 }
 
---
+++ src/components/SocialButton.jsx
@@ -0,0 +1,18 @@
+const SocialButton = ({ link, icon, text }) => {
+  return (
+    <li>
+      <a
+        className=""
+        href={link}
+        target="_blank"
+      >
+        <img
+          className="w-5 h-5 hover:brightness-80"
+          src={icon}
+          alt={text} />
+      </a>
+    </li>
+  )
+}
+
+export default SocialButton
